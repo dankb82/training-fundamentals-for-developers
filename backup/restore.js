41406a0b59834ca272c9889d58df818ea5406918
Daniel Buompane <dbuompane@ebsco.com>
2019-08-06 14:31:10 -0400

    asset deletion and restoration

--- backup/restore.js
+++ backup/restore.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.api.restore(['car', 'motorcycle'])
+cloudinary.api.restore(['jgp6jwdzn8mcmtcw9q7w'])
 .then(restoreResult => console.log(restoreResult))
 .catch(error => console.error(error));
