81bf606bb232569159597fc5c9ce36a88aa1090f
Daniel Buompane <dbuompane@ebsco.com>
2019-08-06 12:14:24 -0400

    pull image from external url

--- upload/lesson1/example8.js
+++ upload/lesson1/example8.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
-cloudinary.uploader.upload('https://upload.wikimedia.org/wikipedia/commons/6/68/Orange_tabby_cat_sitting_on_fallen_leaves-Hisashi-01A.jpg')
+cloudinary.uploader.upload('https://media.mnn.com/assets/images/2018/09/eastern-box-turtle-forest.jpg.1080x0_q100_crop-scale.jpg')
     .then(uploadResult => console.log(uploadResult))
     .catch(error => console.error(error));
