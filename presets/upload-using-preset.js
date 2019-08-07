4f487398e88a1f86cc5930833bb8f16fe531b8b8
Daniel Buompane <dbuompane@ebsco.com>
2019-08-07 09:19:04 -0400

    finish upload basics

--- presets/upload-using-preset.js
+++ presets/upload-using-preset.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.uploader.upload('assets/images/mrwhiskers.jpg', {
-  public_id: 'mrswhiskers',
-  upload_preset: 'my_awesome_preset'
+  public_id: 'drwhiskers',
+  upload_preset: 'c0cqxzqw'
 })
 .then(uploadResult => console.log(uploadResult))
 .catch(error => console.error(error));
